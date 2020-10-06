78d587c3661605dc402012c264ee4d8592343c2a
Matthew Caron <matt@mattcaron.net>
2020-10-05 22:04:04 -0400

    Basic file read skeleton.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+/// Command line arguments
+#[derive(StructOpt)]
+struct Args {
+    /// (Input) the file containing our list of URLs
+    #[structopt(parse(from_os_str))]
+    file: PathBuf,
+}
+
+/// Main function.
 fn main() {
-    println!("Hello, world!");
+    let args = Args::from_args();
+
+    let file = File::open(args.file).expect("Could not read input file.");
+    let reader = BufReader::new(file);
+
+    let lines: Vec<String> = reader.lines().map(|line| line.expect("Error reading line")).collect();
+
+    println!("File contents:\n{:?}", lines)
 }
