8606fc0839b8e45f360ee911d59571ea18ef6206
Matthew Caron <matt@mattcaron.net>
2020-10-18 13:36:11 -0400

    Run through cargo fmt.

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ use std::io::{prelude::*, BufReader};
 use std::path::PathBuf;
 use std::sync::{Arc, Mutex};
 use structopt::StructOpt;
-use tokio::task;
 use tokio::runtime::Runtime;
+use tokio::task;
 
 /// Command line arguments
 #[derive(StructOpt)]
@@ -19,31 +19,32 @@ struct Args {
 ///
 /// # Arguments
 /// * file - Name of file to open and parse
-/// 
+///
 /// # Returns
 /// * On success, an Ok() containing a Vector URLs as strings.
 /// * On Failure, an Err() containing (potentially) useful information is returned.
-/// 
+///
 fn read_file(filename: PathBuf) -> Result<Vec<String>, Box<dyn Error>> {
-
     let file = File::open(filename).expect("Could not read input file.");
     let reader = BufReader::new(file);
 
-    let urls: Vec<String> = reader.lines().map(|line| line.expect("Error reading line")).collect();
+    let urls: Vec<String> = reader
+        .lines()
+        .map(|line| line.expect("Error reading line"))
+        .collect();
 
     return Ok(urls);
 }
 
 /// Process a given URL
-/// 
+///
 /// # Arguments
 /// * url - URL for which we should get the HTML and generate appropriate output
-/// 
+///
 /// # Returns
 /// * On success, an empty Ok() is returned.
 /// * On Failure, an Err() containing (potentially) useful information is returned.
-fn process_url(url: &String, ingredients: Arc<Mutex<Vec<String>>>) ->  Result<(), Box<dyn Error>> {
-    
+fn process_url(url: &String, ingredients: Arc<Mutex<Vec<String>>>) -> Result<(), Box<dyn Error>> {
     let resp = reqwest::blocking::get(url)?.text()?;
 
     println!("Response is: {:?}", resp);
@@ -57,12 +58,12 @@ fn process_url(url: &String, ingredients: Arc<Mutex<Vec<String>>>) ->  Result<()
 ///
 /// # Arguments
 /// * ingredients - Vector of ingredients to be processed
-/// 
+///
 /// # Returns
 /// * On success, an empty Ok() is returned.
 /// * On Failure, an Err() containing (potentially) useful information is returned.
 ///
-fn process_ingredients(ingredients: Vec<String>) ->  Result<(), Box<dyn Error>> {
+fn process_ingredients(ingredients: Vec<String>) -> Result<(), Box<dyn Error>> {
     for ingredient in ingredients {
         // TODO - more
         println!("{:?}", ingredient);
@@ -75,12 +76,12 @@ fn process_ingredients(ingredients: Vec<String>) ->  Result<(), Box<dyn Error>> 
 ///
 /// # Arguments
 /// * urls - Vector of URLs for which we should get and process the HTML
-/// 
+///
 /// # Returns
 /// * On success, an empty Ok() is returned.
 /// * On Failure, an Err() containing (potentially) useful information is returned.
 ///
-async fn get_urls(urls: Vec<String>) ->  Result<(), Box<dyn Error>> {
+async fn get_urls(urls: Vec<String>) -> Result<(), Box<dyn Error>> {
     let mut tasks: Vec<task::JoinHandle<_>> = Vec::new();
     let ingredients: Arc<Mutex<Vec<String>>> = Arc::new(Mutex::new(Vec::new()));
 
@@ -88,7 +89,8 @@ async fn get_urls(urls: Vec<String>) ->  Result<(), Box<dyn Error>> {
         // New variable to receive clone before being moved into the function
         let my_ingredient = ingredients.clone();
         tasks.push(tokio::spawn(async move {
-            process_url(&url, my_ingredient).expect(format!("Error processing URL: {}", url).as_str());
+            process_url(&url, my_ingredient)
+                .expect(format!("Error processing URL: {}", url).as_str());
         }));
     }
 
@@ -100,11 +102,10 @@ async fn get_urls(urls: Vec<String>) ->  Result<(), Box<dyn Error>> {
     process_ingredients(ingredients.lock().unwrap().to_vec())?;
 
     return Ok(());
-
 }
 
 /// Main function
-/// 
+///
 /// # Returns
 /// * On success, an empty Ok() is returned.
 /// * On Failure, an Err() containing (potentially) useful information is returned.
